97d9edf8d0abb872435b5d3b2ea71ac395f0c15f
Rizqika Mulia Pratama <rizqikapratamaa@gmail.com>
2025-03-17 20:51:58 +0700

    add: node class

---
+++ Node.py
@@ -0,0 +1,36 @@
+import random
+import math
+
+class Node:
+    def __init__(self, value, gradient = 0.0):
+        self.value = value # node value
+        self.gradient = gradient #node gradient
+        self.parent = [] #list of parent node, it contains tuple (parent_node, local_gradient)
+        self.op = None #operation that produce this node
+    
+    def __add__(self, other):
+        other = other if isinstance(other, Node) else Node(other)
+        result = Node(self.value + other.value)
+        result.parent = [(self, 1.0), other, 1.0] # gradient to its parents
+        self.op = "add"
+        return result
+    
+    def __mul__(self, other):
+        other = other if isinstance(other, Node) else Node(other)
+        result = Node(self.value * other.value)
+        result.parent = [(self, other.value), (other, self.value)] #local gradient
+        result.op = "mul"
+        return result
+    
+    def sigmoid(self):
+        value = 1 / (1 + math.exp(-self.value))
+        result = Node(value)
+        result.parent = [(self, value * (1 - value))]
+        result.op = "sigmoid"
+        return value
+    
+    def backward(self, gradient = 1.0):
+        self.gradient += gradient
+        for parent, local_gradient in self.parent:
+            if parent.op:
+                parent.backward(gradient + local_gradient)
